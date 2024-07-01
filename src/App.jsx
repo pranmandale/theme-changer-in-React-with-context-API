b33f8053c790d512d1c43d5482e985b5a13a2d59
pranmandale <pranavmandale2025@gmail.com>
2024-07-01 23:28:33 +0530

    new files added

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+import ThemeBtn from './components/ThemeBtn'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='flex flex-wrap min-h-screen items-center'>
+     <div className='w-full'>
+     <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
+        <ThemeBtn/>
+     </div>
+     <div className='w-full max-w-sm mx-auto'>
+      <Card/>
+     </div>
+
+     </div>
+
+     </div>
+    </>
+  )
+}
+
+export default App
